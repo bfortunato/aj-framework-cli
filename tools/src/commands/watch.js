764e7b62d6e4b1b9b5c9669bae685e33af22c503
Bruno Fortunato <bruno.fortunato@applica.guru>
2016-10-14 18:34:19 +0200

    Bug Fixing

--- tools/src/commands/watch.js
+++ tools/src/commands/watch.js
@@ -55,7 +55,7 @@ function transpile(sourceFile) {
                 }
             });
 
-            log(sourceFile + " compiled!");
+            log("[COMPILED] " + sourceFile);
         }
     });
 }
