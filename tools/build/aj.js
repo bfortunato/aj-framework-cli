03f02fd76017f64482665530c96578d2f3aabfb1
Bruno Fortunato <bruno.fortunato@applica.guru>
2016-11-22 21:06:11 +0100

    Bug fixing

--- tools/build/aj.js
+++ tools/build/aj.js
@@ -12,7 +12,7 @@ function list(val) {
     return val.split(',');
 }
 
-program.version("1.0.2");
+program.version("1.0.4");
 
 program.command("init <path>").description("Creates a new AJ project in specified path").action(function (path) {
     (0, _commands.init)(path);
--- tools/src/aj.js
+++ tools/src/aj.js
@@ -10,7 +10,7 @@ function list(val) {
 }
 
 program
-    .version("1.0.2")
+    .version("1.0.4")
 
 program
     .command("init <path>")
