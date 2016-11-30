8281069aaf2fe5a8af402f0cca9b1dd65081b89c
Bruno Fortunato <bruno.fortunato@applica.guru>
2016-11-30 09:31:42 +0100

    Bug fixing

--- tools/build/commands/build.js
+++ tools/build/commands/build.js
@@ -372,15 +372,15 @@ function generateActions(cb) {
         var matches = res.match(reg);
 
         matches.forEach(function (m) {
-            var action = m.replace("createAction(", "");
+            var action = m.replace("createAction(", "").replace("types.", "");
             actions.push(action);
         });
 
-        var iosCode = "\n//\n//  Actions.swift\n//\n//  Auto generated from aj build\n//\n\nimport foundation\n        \nstruct Actions {\n" + actions.map(function (s) {
+        var iosCode = "\n//\n//  Actions.swift\n//\n//  Auto generated from aj build\n//\n\nimport Foundation\n        \nstruct Actions {\n" + actions.map(function (s) {
             return "\tstatic let " + s + " = \"" + s + "\"";
         }).join("\n") + "\n}\n";
 
-        var androidCode = "\n//\n//  Actions.java\n//\n//  Auto generated from aj build\n//\n\npackage applica.app;\n        \nclass Actions {\n" + actions.map(function (s) {
+        var androidCode = "\n//\n//  Actions.java\n//\n//  Auto generated from aj build\n//\n\npackage applica.app;\n        \npublic class Actions {\n" + actions.map(function (s) {
             return "\tpublic static final String " + s + " = \"" + s + "\";";
         }).join("\n") + "\n}\n";
 
@@ -402,15 +402,15 @@ function generateStores(cb) {
         var matches = res.match(reg);
 
         matches.forEach(function (m) {
-            var store = m.replace("createStore(", "");
+            var store = m.replace("createStore(", "").replace("types.", "");
             stores.push(store);
         });
 
-        var iosCode = "\n//\n//  Stores.swift\n//\n//  Auto generated from aj build\n//\n\nimport foundation\n        \nstruct Stores {\n" + stores.map(function (s) {
+        var iosCode = "\n//\n//  Stores.swift\n//\n//  Auto generated from aj build\n//\n\nimport Foundation\n        \nstruct Stores {\n" + stores.map(function (s) {
             return "\tstatic let " + s + " = \"" + s + "\"";
         }).join("\n") + "\n}\n";
 
-        var androidCode = "\n//\n//  Stores.java\n//\n//  Auto generated from aj build\n//\n\npackage applica.app;\n        \nclass Stores {\n" + stores.map(function (s) {
+        var androidCode = "\n//\n//  Stores.java\n//\n//  Auto generated from aj build\n//\n\npackage applica.app;\n        \npublic class Stores {\n" + stores.map(function (s) {
             return "\tpublic static final String " + s + " = \"" + s + "\";";
         }).join("\n") + "\n}\n";
 
--- tools/src/aj.js
+++ tools/src/aj.js
@@ -10,7 +10,7 @@ function list(val) {
 }
 
 program
-    .version("1.0.7")
+    .version("aj-framework-cli v1.0.8")
 
 program
     .command("init <path>")
